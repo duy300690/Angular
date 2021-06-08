885473217bf50e71756e679ae48640960f2790dc
duy300690 <duy300690@gmail.com>
2021-06-08 10:51:24 +0700

    update

--- angular-v12/src/app/home/home.component.ts
+++ angular-v12/src/app/home/home.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public name: string = 'Minh Duy';
+  public age: number = 28;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public resetForm(): void {
+    this.name = '';
+    this.age = 0;
   }
-
 }
